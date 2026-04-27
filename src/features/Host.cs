57e84850b600659d88d0b1905bbc2acb3120412f
Mr Diamond <49098391+MrDiamond64@users.noreply.github.com>
2026-04-26 21:57:59 -0700

    feat: Add back Flip Skeld map option
    The old implemtation of the Flip Skeld feature would patch the AprilFoolsMode::ShouldFlipSkeld function to always return true. In Among Us version 17.3, the AprilFoolsMode::ShouldFlipSkeld function was updated to always return false meaning the flipped skeld map will no longer appear on April 1st. This shouldn't be too much of a problem, as we are patching the function to always return true, at least in theory. Thanks to il2cpp compiler optimizations, not only will patching that function bring back the flipped skeld map, it will also bring along a lot of unintended side affects that break the Among Us game
    
    When il2cpp is optimizing a binary, it will look to see if there are functions that have the same return values and combine them into one singular function. If we had a function called 'Test1' and 'Test2' that both returns the integer value of 2, il2cpp will reroute calls of Test2 into Test1 and delete the Test2 function, helping to reduce the output binary size. The same thing applies here. Since AprilFoolsMode::ShouldFlipSkeld and a large list of other functions return false, the il2cpp compiler will merge these functions into one, so by patching AprilFoolsMode::ShouldFlipSkeld to return true, we are also making any other function that also returns false to now return true.
    
    This will cause a lot of game breaking bugs, such as the game being stuck on a black screen in some situations, other april fool toggles such as Horse Mode being enabled, being able to complete tasks while dead, always being a ghost, HnS overlays always showing up, etc. To deal with this, we will have to use a brand new method to get the flipped skeld map to appear. When the host starts an Among Us game, their client will fetch the proper map through the AmongUsClient.ShipPrefabs list. This list corresponds map IDs to the proper maps. List index 0 would return the map for The Skeld, list index 1 would return the map for Mira HQ, list index 3 would return Dleks ehT, etc. We can remap map IDs to different map assets in order to change which map ID spawns in which map, so instead of map ID 0 loading in The Skeld, it will instead load Dleks ehT.
    
    Acknowledgements: scp222thj - for letting me know that part of the il2cpp compiler optimizations will merge similiar functions together

--- src/features/Host.cs
+++ src/features/Host.cs
@@ -1,10 +1,31 @@
 ﻿using HarmonyLib;
 using InnerNet;
+using UnityEngine.AddressableAssets;
 
 namespace HydraMenu.features
 {
 	internal class Host
 	{
+		private static bool isSkeldFlipped = false;
+		public static bool FlippedSkeld
+		{
+			get { return isSkeldFlipped; }
+			set
+			{
+				if(AmongUsClient.Instance == null || isSkeldFlipped == value) return;
+
+				// ShipPrefabs is a list corresponding map IDs to their map
+				// ID 0 is Skeld, 1 is Mira, 2 is Polus, and 3 is Dleks
+				// If we want to be able to spawn in Dleks (as this is normally inaccessible) we can swap the two elements
+				// so that 0 is Dleks and 3 is Skeld, spawning in Dleks instead of Skeld
+				AssetReference temp = AmongUsClient.Instance.ShipPrefabs[3];
+				AmongUsClient.Instance.ShipPrefabs[3] = AmongUsClient.Instance.ShipPrefabs[0];
+				AmongUsClient.Instance.ShipPrefabs[0] = temp;
+
+				isSkeldFlipped = value;
+			}
+		}
+
 		// When a player reports a body, their client sends a ReportDeadBody RPC to the host. The host then should validate the RPC and start a meeting
 		// To block meetings, we can simply ignore any received ReportDeadBody RPCs
 		[HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.ReportDeadBody))]
